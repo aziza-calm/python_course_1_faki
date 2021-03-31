579820825e4f253e03faa0188d5556e1ddde16cd
aziza <aziza.kalmasova@phystech.edu>
2021-03-31 16:25:25 +0300

    string methods tests added

--- pyqt_games/main.py
+++ pyqt_games/main.py
@@ -1,3 +1,4 @@
+import os
 import window
 from PyQt5 import QtWidgets
 
@@ -6,12 +7,21 @@ class Example(QtWidgets.QMainWindow, window.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.browse_folder)
+
+    def browse_folder(self):
+        self.listWidget.clear()
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+
+        if directory:
+            for file_name in os.listdir(directory):
+                self.listWidget.addItem(file_name)
 
 
 def main():
     app = QtWidgets.QApplication([])
-    window = Example()
-    window.show()
+    main_window = Example()
+    main_window.show()
     app.exec()
 
 
