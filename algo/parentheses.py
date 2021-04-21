9d34ebb93d2dc6911a5c1f1d1d6d55d470625ddf
aziza <aziza.kalmasova@phystech.edu>
2021-04-21 18:50:52 +0300

    скобочные последовательности: finished

--- algo/parentheses.py
+++ algo/parentheses.py
@@ -1,10 +1,11 @@
 def is_valid(s):
     """
-    Функция, проверяющая правильность скобочной последовательности
+    Функция, проверяющая правильность скобочной последовательности. Алгоритм с испльзованием стека.
     :param s: str - скобочная последовательность. Например, "(){}[]"
     :return: bool
     """
     stack = []
+    mapping = {")": "(", "]": "[", "}": "{"}
     for char in s:
         if char == "(":
             stack.append(char)
@@ -14,5 +15,20 @@ def is_valid(s):
     return not stack
 
 
+def is_valid_str(s):
+    """
+    Функция, проверяющая правильность скобочной последовательности.
+    Алгоритм с использованием строковых методов.
+    :param s: str - скобочная последовательность. Например, "(){}[]"
+    :return: bool
+    """
+    while "()" in s or "{}" in s or "[]" in s:
+        s = s.replace("{}", "").replace("()", "").replace("[]", "")
+    return s == ""
+
+
 if __name__ == "__main__":
-    print(is_valid("((((((()))"))
+    print(is_valid_str("(]"))
+    print(is_valid_str("(]]]]]"))
+    print(is_valid_str("{(}"))
+    print(is_valid_str("[[]]"))
